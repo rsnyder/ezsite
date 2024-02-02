e4773dca99695b5eff71b5a61f9448edfdb548a4
Ron Snyder <ron@snyderjr.com>
2024-02-01 20:45:32 -0500

    Download modal styling

--- ezsite/src/components/Menu.ce.vue
+++ ezsite/src/components/Menu.ce.vue
@@ -24,7 +24,7 @@
     <div ref="modalRef" id="hs-basic-modal" class="hs-overlay hidden w-full h-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none">
       <div class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
         <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-          <div class="p-4 overflow-y-auto flex items-center gap-4">
+          <div class="p-4 overflow-y-auto flex items-center gap-4 text-black">
             <sl-spinner style="font-size: 3rem; --indicator-color: deeppink; --track-color: pink;"></sl-spinner>
             <p v-html="modalText"></p>
           </div>
