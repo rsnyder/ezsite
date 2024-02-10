df96ba44d92eee7909340716fb58ca45e42949e5
Ron Snyder <ron@snyderjr.com>
2024-02-10 10:46:35 -0500

    testing

--- ezsite/juncture/index.js
+++ ezsite/juncture/index.js
@@ -1,4 +1,6 @@
 
+console.log(location)
+
 const junctureDependencies = [
   // {tag: 'link', rel: 'stylesheet', href: `${config.baseurl}juncture/index.css`},
   {tag: 'link', rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'},
