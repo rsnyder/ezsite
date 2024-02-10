cf5851e76b54e6cb01b414658b6ddacfd4e5bedc
Ron Snyder <ron@snyderjr.com>
2024-02-10 11:23:00 -0500

    testing

--- ezsite/juncture/index.js
+++ ezsite/juncture/index.js
@@ -110,7 +110,7 @@ function _createJunctureV1App() {
   new window.Vue({
     el: '#vue',
     components: {
-      'juncture-v1': window.httpVueLoader(`${config.baseurl}/ezsite/juncture/v1/Juncture.vue`)
+      'juncture-v1': window.httpVueLoader(`${scriptBasePath}/ezsite/juncture/v1/Juncture.vue`)
     },
     data: () => ({ html })
   })
